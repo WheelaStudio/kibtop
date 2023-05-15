52703a5c431845ebed2a745ae0c0fc6d70649bbf
Wheela Studio <wheelastudio@gmail.com>
2023-05-15 17:51:11 +0300

    Revert "Merge pull request #1 from WheelaStudio/ulan"
    This reverts commit f7ce0c353e487064834b96de35c352311f776790, reversing
    changes made to 2eace3e39aa8a7c74b454b159d477fed0442a329.

--- services/frontend/components/AdvertDetailPage/OtherDetail/DetailMaps/Map/Map.js
+++ services/frontend/components/AdvertDetailPage/OtherDetail/DetailMaps/Map/Map.js
@@ -2,60 +2,60 @@ import { GoogleMap, Marker } from "@react-google-maps/api";
 import { useRef } from "react";
 import { useCallback } from "react";
 import { defaultTheme } from "./MapTheme";
-import icon from "../Map/img/mapicon.png";
+
 
 const containerStyle = {
-  width: "100%",
-  height: "100%",
+    width: '100%',
+    height: '100%'
 };
 
 const defaultOptions = {
-  panControl: true,
-  zoomControl: false,
-  mapTypeControl: false,
-  scaleControl: false,
-  streetViewControl: false,
-  rotateControl: false,
-  clickableIcons: false,
-  keyboardShortcuts: false,
-  scrollwheel: true,
-  disableDoubleClickZoom: false,
-  fullscreenControl: false,
-  styles: defaultTheme,
-};
+    panControl: true,
+    zoomControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    clickableIcons: false,
+    keyboardShortcuts: false,
+    scrollwheel: true,
+    disableDoubleClickZoom: false,
+    fullscreenControl: false,
+    styles: defaultTheme
+}
 
-const Map = ({ center }) => {
-  const mapRef = useRef(null);
 
-  const onLoad = useCallback(function callback(map) {
-    mapRef.current = map;
-  }, []);
+const Map = ({center}) => {
 
-  const onUnmount = useCallback(function callback(map) {
-    mapRef.current = null;
-  }, []);
+    const mapRef = useRef(null)
 
-  return (
-    <>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={17}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-        options={defaultOptions}>
-        {/* Child components, such as markers, info windows, etc. */}
+    const onLoad = useCallback(function callback(map) {
+        mapRef.current = map
+      }, [])
+    
+      const onUnmount = useCallback(function callback(map) {
+        mapRef.current = null
+      }, [])
+
+
+    return (
         <>
-          <Marker
-            position={center}
-            // icon={{
-            //   url: { icon },
-            // }}
-          />
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={17}
+                onLoad={onLoad}
+                onUnmount={onUnmount}
+                options={defaultOptions}
+            >
+                { /* Child components, such as markers, info windows, etc. */ }
+                <>
+                    
+                        <Marker position={center} icon={{url: '/img/goods/marker_fake.svg'}} />
+                </>
+            </GoogleMap>
         </>
-      </GoogleMap>
-    </>
-  );
-};
+    );
+}
 
 export default Map;
