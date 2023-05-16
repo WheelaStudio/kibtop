547bc53269abe0e5ab705caf3d06d3194cdc2bd3
UlanTol <104356514+UlanTol@users.noreply.github.com>
2023-05-16 23:14:22 +0300

    fields

--- services/frontend/components/AddAdvertPage/AddAdvertForm/Fields/MileageField.js
+++ services/frontend/components/AddAdvertPage/AddAdvertForm/Fields/MileageField.js
@@ -3,28 +3,37 @@ import { useLanguage } from "../../../../locales/hooks/useLanguage";
 import Text from "../../../Elementes/Text/Text";
 
 const MileageField = () => {
-    const {t} = useLanguage()
-    const {register, formState, getFieldState} = useFormContext()
-    const {error, isTouched} = getFieldState('mileage', formState)
-    
-    return (
-        <>
-            <div className="advert-form__text-input">
-                <input {...register('mileage', {
-                        required: 'field is required',
-                        pattern: {
-                            value: /^[1-9]{1}\d*$/,
-                            message: 'invalid format'
-                        }
-                    })}
-                    type="text" 
-                    placeholder={t('Mileage in kilometers')} 
-                    className={"empty-field empty-field--full" + ((!!error && isTouched) ? ' field--error': '')} />
+  const { t } = useLanguage();
+  const { register, formState, getFieldState } = useFormContext();
+  const { error, isTouched } = getFieldState("mileage", formState);
 
-                { (isTouched && !!error && !!error.message) && <p className="warn warn--absolute"><Text content={error.message} /></p> }
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="advert-form__text-input">
+        <input
+          {...register("mileage", {
+            required: "field is required",
+            pattern: {
+              value: /^[1-9]{1}\d*$/,
+              message: "invalid format",
+            },
+          })}
+          type="text"
+          placeholder={t("Mileage in kilometers")}
+          className={
+            "empty-field empty-field--full-changed" +
+            (!!error && isTouched ? " field--error" : "")
+          }
+        />
+
+        {isTouched && !!error && !!error.message && (
+          <p className="warn warn--absolute">
+            <Text content={error.message} />
+          </p>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default MileageField;
