bd030148ed61a3fb49a030e29d7dc6a276702fc0
UlanTol <104356514+UlanTol@users.noreply.github.com>
2023-05-23 19:03:19 +0300

    rec correction

--- services/frontend/components/RecoveryPage/RecoveryForm/RecoveryFormContainer.js
+++ services/frontend/components/RecoveryPage/RecoveryForm/RecoveryFormContainer.js
@@ -1,18 +1,23 @@
-import { useDispatch, useSelector } from "react-redux"
-import { sendRecoveryLinkThunk } from "../../../store/slices/RecoverySlice"
-import RecoveryForm from "./RecoveryForm"
+import { useDispatch, useSelector } from "react-redux";
+import { sendRecoveryLinkThunk } from "../../../store/slices/RecoverySlice";
+import RecoveryForm from "./RecoveryForm";
 
 const RecoveryFormContainer = () => {
-    const {isLoading, isUserFail} = useSelector(state => state.recovery)
-    const dispatch = useDispatch()
+  const { isLoading, isUserFail } = useSelector(state => state.recovery);
+  const dispatch = useDispatch();
 
-    const onRecoverySubmit = data => {
-        const {email} = data
-        dispatch(sendRecoveryLinkThunk(email))
-    }
+  const onRecoverySubmit = data => {
+    const { email } = data;
+    dispatch(sendRecoveryLinkThunk(email));
+  };
 
-
-    return <RecoveryForm {...{isLoading, onRecoverySubmit, isUserFail}} />;
-}
+  return (
+    <RecoveryForm
+      isLoading={isLoading}
+      onRecoverySubmit={onRecoverySubmit}
+      isUserFail={isUserFail}
+    />
+  );
+};
 
 export default RecoveryFormContainer;
