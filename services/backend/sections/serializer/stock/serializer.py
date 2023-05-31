3d7865b7f001b7519bd235a392f5a27dbc468dc8
Dmitry Lukyanov <vjick7@gmail.com>
2023-05-31 22:55:53 +0300

    Доработка Stock - Фото и мобильное фото по языкам.

--- services/backend/sections/serializer/stock/serializer.py
+++ services/backend/sections/serializer/stock/serializer.py
@@ -6,19 +6,22 @@ from sections.models import Stock
 class StockSerializerEN(serializers.ModelSerializer):
     class Meta:
         model = Stock
-        fields = ('id', 'isDark', 'img', 'title_en', 'desc_en', 'background', 'link')
+        #fields = ('id', 'isDark', 'img', 'title_en', 'desc_en', 'background', 'link')
+        fields = ('id', 'title_en', 'image_en', 'image_mobile_en', 'link')
 
 
 class StockSerializerRU(serializers.ModelSerializer):
     class Meta:
         model = Stock
-        fields = ('id', 'isDark', 'img', 'title_ru', 'desc_ru', 'background', 'link')
+        #fields = ('id', 'isDark', 'img', 'title_ru', 'desc_ru', 'background', 'link')
+        fields = ('id', 'title_ru', 'image_ru', 'image_mobile_ru', 'link')
 
 
 class StockSerializerTR(serializers.ModelSerializer):
     class Meta:
         model = Stock
-        fields = ('id', 'isDark', 'img', 'title_tr', 'desc_tr', 'background', 'link')
+        #fields = ('id', 'isDark', 'img', 'title_tr', 'desc_tr', 'background', 'link')
+        fields = ('id', 'title_tr', 'image_tr', 'image_mobile_tr', 'link')
 
 
 class StockSerializer(serializers.Serializer):
