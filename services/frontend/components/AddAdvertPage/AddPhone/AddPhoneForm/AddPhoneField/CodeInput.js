86ad445f22c8577b02137c3b09aa10beaa754470
UlanTol <104356514+UlanTol@users.noreply.github.com>
2023-06-08 18:03:17 +0300

    before editAd

--- services/frontend/components/AddAdvertPage/AddPhone/AddPhoneForm/AddPhoneField/CodeInput.js
+++ services/frontend/components/AddAdvertPage/AddPhone/AddPhoneForm/AddPhoneField/CodeInput.js
@@ -1,29 +1,35 @@
-import { getCountries, getCountryCallingCode } from "react-phone-number-input";
-import { useBoolState } from "../../../../AppHooks/useBoolState";
-import style from "./add_tel.module.scss"
-import Codes from "./Codes";
+// import { CountrySelect } from "react-phone-number-input";
+// import { useBoolState } from "../../../../AppHooks/useBoolState";
+// import style from "./add_tel.module.scss";
+// import Codes from "./Codes";
 
-const CodeInput = ({code, setCode, nextInput}) => {
-    const [isOpen, setOpen] = useBoolState()
-    const onChange = e => {
-        
-        const input = e.currentTarget.value
-        if(input.length > 3) {
-            setOpen(false)
-            nextInput.current.focus()
-            return
-        }
-        if(!isNaN(+input)) setCode(input)
-        
-        setOpen(!!input)
-    }
+// const CodeInput = ({ code, setCode, nextInput }) => {
+//   const [isOpen, setOpen] = useBoolState();
 
-    return (
-        <>
-            
-            
-        </>
-    );
-}
+//   return (
+//     <>
+//       <div className={style.wrapper}>
+//         <Codes
+//           search={code}
+//           setValue={setCode}
+//           {...{
+//             isOpen,
+//             setOpen,
+//             nextInput: () => nextInput.current.focus(),
+//           }}
+//         />
 
-export default CodeInput;
+//         <div className={style.code}>
+//           <CountrySelect
+//             value={code}
+//             onChange={setCode}
+//             onBlur={() => setOpen(false)}
+//             onFocus={() => setOpen(true)}
+//           />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default CodeInput;
--- services/frontend/components/EditProfilePage/EditProfileForm/EditProfileFormContainer.js
+++ services/frontend/components/EditProfilePage/EditProfileForm/EditProfileFormContainer.js
@@ -1,34 +1,42 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { editUserDataThunk, setProfileDataThunk, setProfileEditingActivated, setProfileEditingSuccess } from "../../../store/slices/ProfileSlice";
+import {
+  editUserDataThunk,
+  setProfileDataThunk,
+  setProfileEditingActivated,
+  setProfileEditingSuccess,
+} from "../../../store/slices/ProfileSlice";
 import EditProfileForm from "./EditProfileForm";
 
-const EditProfileFormContainer = ({user}) => {
-    const {email, name, city, avatar, isSuccess, isLoading, isActivated} = useSelector(state => state.profile)
-    const {push} = useRouter()
-    const dispatch = useDispatch()
+const EditProfileFormContainer = ({ user }) => {
+  const { email, name, city, avatar, isSuccess, isLoading, isActivated } =
+    useSelector(state => state.profile);
+  const { push } = useRouter();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if(isActivated) {
-            dispatch(setProfileEditingActivated(false))
-            dispatch(setProfileEditingSuccess(false))
-            push('/profile')
-        }
-        if(!email || !name || !city || !avatar) dispatch(setProfileDataThunk())
-    }, [email, name, city, avatar, isActivated])
-
-    const onEditProfileSubmit = data => {
-        
-        const {email, name, city, file, phoneNumber, phoneCode} = data
-        const phone = `+${phoneCode}${phoneNumber}`
-        dispatch(editUserDataThunk({email, name, city, avatar: file[0], phone}))
+  useEffect(() => {
+    if (isActivated) {
+      dispatch(setProfileEditingActivated(false));
+      dispatch(setProfileEditingSuccess(false));
+      push("/profile");
     }
+    if (!email || !name || !city || !avatar) dispatch(setProfileDataThunk());
+  }, [email, name, city, avatar, isActivated]);
 
-    const userData = user || { email, name, city, avatar }
+  const onEditProfileSubmit = data => {
+    const { email, name, city, file, phoneNumber, phoneCode } = data;
+    const phone = `+${phoneNumber}`;
+    dispatch(editUserDataThunk({ email, name, city, avatar: file[0], phone }));
+  };
 
+  const userData = user || { email, name, city, avatar };
 
-    return <EditProfileForm {...{onEditProfileSubmit, isLoading, isSuccess, ...userData}} />;
-}
+  return (
+    <EditProfileForm
+      {...{ onEditProfileSubmit, isLoading, isSuccess, ...userData }}
+    />
+  );
+};
 
 export default EditProfileFormContainer;
